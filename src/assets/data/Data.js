4517c61dbb44d498e2dd862d42bbe357de5fd968
Reihane <chamanireihane@gmail.com>
2025-06-19 15:01:52 +0330

    ...

--- src/assets/data/Data.js
+++ src/assets/data/Data.js
@@ -1,12 +1,110 @@
+import {TbExchange} from "react-icons/tb";
+import React from "react";
+
 export const indexHead = [
     {
         title : "ویرایش و پردازش متن فارسی",
         description : "متون فارسی خود را ویرایش و بهینه کنید.",
-        color : "bg-indigo-500",
+        color1 : "bg-emerald-600",
+        color2 : "bg-emerald-200",
+        item : [
+            {
+                title : "تبدیل کارکتر های فارسی به عربی",
+                description : "تغییر حروف عربی در نوشته های فارسی به کارکتر های فارسی.",
+                link : "/convert-persian-characters-to-arabic",
+                color1 : "bg-emerald-400",
+                color2 : "bg-emerald-50",
+                textColor1 : "text-emerald-500",
+                icon : <TbExchange size={30}/>,
+            },
+            {
+                title : "ابزار حذف نیم‌فاصله از متن",
+                description : "حذف نیم فاصله های متن ها",
+                link : "/zero-width-space-remover",
+                color1 : "bg-emerald-400",
+                color2 : "bg-emerald-50",
+                textColor1 : "text-emerald-500",
+                icon : <TbExchange size={30}/>,
+            },
+            {
+                title : "ابزار رفع کشیدگی متن",
+                description : "از بین بردن کشیدگی های حروف در متن ها.",
+                link : "/tatweel-remover",
+                color1 : "bg-emerald-400",
+                color2 : "bg-emerald-50",
+                textColor1 : "text-emerald-500",
+                icon : <TbExchange size={30}/>,
+            },
+        ]
     },
     {
         title : "برنامه نویسان و توسعه دهندگان",
         description : "داده ها را بسازید و اصلاح کنید",
-        color : "bg-orange-500",
+        color1 : "bg-rose-800",
+        color2 : "bg-red-200",
+        item : [
+            {
+                title : "تبدیل کارکتر های فارسی به عربی",
+                description : "تغییر حروف عربی در نوشته های فارسی به کارکتر های فارسی.",
+                link : "/convert-persian-characters-to-arabic",
+                color1 : "bg-rose-700",
+                color2 : "bg-rose-50",
+                textColor1 : "text-rose-500",
+                icon : <TbExchange size={30}/>,
+            },
+            {
+                title : "ابزار حذف نیم‌فاصله از متن",
+                description : "حذف نیم فاصله های متن ها",
+                link : "/zero-width-space-remover",
+                color1 : "bg-rose-700",
+                color2 : "bg-rose-50",
+                textColor1 : "text-rose-500",
+                icon : <TbExchange size={30}/>,
+            },
+            {
+                title : "ابزار رفع کشیدگی متن",
+                description : "از بین بردن کشیدگی های حروف در متن ها.",
+                link : "/tatweel-remover",
+                color1 : "bg-rose-700",
+                color2 : "bg-rose-50",
+                textColor1 : "text-rose-500",
+                icon : <TbExchange size={30}/>,
+            },
+        ]
+    },
+    {
+        title : "محاسبات زندگی روزمره",
+        description : "آنلاین هرچیزی را محاسبه کنید",
+        color1 : "bg-cyan-700",
+        color2 : "bg-cyan-200",
+        item : [
+            {
+                title : "تبدیل کارکتر های فارسی به عربی",
+                description : "تغییر حروف عربی در نوشته های فارسی به کارکتر های فارسی.",
+                link : "/convert-persian-characters-to-arabic",
+                color1 : "bg-cyan-500",
+                color2 : "bg-cyan-50",
+                textColor1 : "text-cyan-500",
+                icon : <TbExchange size={30}/>,
+            },
+            {
+                title : "ابزار حذف نیم‌فاصله از متن",
+                description : "حذف نیم فاصله های متن ها",
+                link : "/zero-width-space-remover",
+                color1 : "bg-cyan-500",
+                color2 : "bg-cyan-50",
+                textColor1 : "text-cyan-500",
+                icon : <TbExchange size={30}/>,
+            },
+            {
+                title : "ابزار رفع کشیدگی متن",
+                description : "از بین بردن کشیدگی های حروف در متن ها.",
+                link : "/tatweel-remover",
+                color1 : "bg-cyan-500",
+                color2 : "bg-cyan-50",
+                textColor1 : "text-cyan-500",
+                icon : <TbExchange size={30}/>,
+            },
+        ]
     },
 ]
--- src/components/pages/IndexHead.js
+++ src/components/pages/IndexHead.js
@@ -2,30 +2,137 @@
 import React, {useEffect, useState} from 'react';
 import {indexHead} from "@/assets/data/Data";
 import Link from "next/link";
-import {BiCalendar} from "react-icons/bi";
 
 const IndexHead = () => {
 
     const [number,setNumber] = useState(0)
+    const count = indexHead.length - 1
     const nextImage = () => {
-        if (number === indexHead.length - 1){
-            setNumber(0)
-        } else {
-            setNumber(number + 1)
-        }
+        setNumber(number < count ? number + 1 : 0)
     }
+    let a = 'translate-y-full z-10 hidden'
+    let b = 'translate-x-full z-10 hidden'
     useEffect(() => {
         const interval = setInterval(() => {
             nextImage()
-        }, 2000);
+        }, 4000);
         return () => clearInterval(interval);
     }, [number,setNumber]);
 
     return (
-        <div className={`flex flex-col items-center justify-center gap-10`}>
-            <h1 className={`font-bold text-gray-800  text-5xl`}> ابزارهای رایگان برای<span className={`${indexHead[number].color} flex flex-col text-white/90 p-2`}>{indexHead[number].title}</span></h1>
-            <h2 className={`text-gray-500`}>در Dtools به سادگی {indexHead[number].description} همه‌چیز رایگان، سریع و با کیفیت و حرفه‌ای در دسترس شماست.</h2>
+        <div className={`flex flex-col gap-20 max-w-screen-xl mx-auto w-full items-center justify-center`}>
+            <div className={`flex flex-col items-center justify-center gap-10`}>
+                <h1 className={` items-center font-bold text-gray-800 flex flex-row gap-1.5 text-5xl`}>
+                    <span>ابزارهای رایگان برای</span>
+                    <span className={`flex flex-col relative h-20 w-180 overflow-hidden`}>
+                        {indexHead.map((value, index) => {
+                            if(number === index){
+                                a = 'translate-y-0 z-20'
+                            }else {
+                                if(number - 1 === index || number + 1 === index){
+                                    if(number - index > 0){
+                                        a = '-translate-y-full z-10'
+                                    }else {
+                                        a = 'translate-y-full z-10'
+                                    }
+                                }
+                                if(number === 0){
+                                    if(index === count){
+                                        a = '-translate-y-full z-10'
+                                    }
+                                }
+                                if(number === count){
+                                    if(index === 0){
+                                        a = 'translate-y-full z-10'
+                                    }
+                                }
+                            }
+                            return (
+                                <span key={index} className={` ${a} ${value.color1} absolute anime_hover duration-500 w-180 text-center text-white/90 px-3 py-4`}>{value.title}</span>
+                            )
+                        })}
+                    </span>
+                </h1>
+                <h2 className={` items-center flex flex-row gap-1.5 text-gray-500`}>
+                    <span>در Dtools به سادگی</span>
+                    <span className={`flex flex-col relative h-10 w-70 overflow-hidden`}>
+                        {indexHead.map((value, index) => {
+                            if(number === index){
+                                a = 'translate-y-0 z-20'
+                            }else {
+                                if(number - 1 === index || number + 1 === index){
+                                    if(number - index > 0){
+                                        a = '-translate-y-full z-10'
+                                    }else {
+                                        a = 'translate-y-full z-10'
+                                    }
+                                }
+                                if(number === 0){
+                                    if(index === count){
+                                        a = '-translate-y-full z-10'
+                                    }
+                                }
+                                if(number === count){
+                                    if(index === 0){
+                                        a = 'translate-y-full z-10'
+                                    }
+                                }
+                            }
+                            return (
+                                <span key={index} className={`w-70 absolute anime_hover ${value.color2} duration-500 ${a} text-center p-2`}>{value.description}</span>
+                            )
+                        })}
+                    </span>
+                    <span>همه‌چیز رایگان، سریع و با کیفیت و حرفه‌ای در دسترس شماست.</span>
+                </h2>
+            </div>
+            <div className={`relative h-120 w-full overflow-hidden max-w-screen-lg mx-auto flex flex-row`}>
+                {indexHead.map((value, index) => {
+                    if(number === index){
+                        b = 'translate-x-0 z-20'
+                    }else {
+                        if(number - 1 === index || number + 1 === index){
+                            if(number - index > 0){
+                                b = '-translate-x-full z-10'
+                            }else {
+                                b = 'translate-x-full z-10'
+                            }
+                        }
+                        if(number === 0){
+                            if(index === count){
+                                b = '-translate-x-full z-10'
+                            }
+                        }
+                        if(number === count){
+                            if(index === 0){
+                                b = 'translate-x-full z-10'
+                            }
+                        }
+                    }
+                    return (
+                        <div key={index} className={`absolute ${b} anime_hover duration-500 flex flex-row gap-8`}>
+                            {value.item?.map((value, index) => {
+                                return (
+                                    <div key={index} className={`shadow rounded-xl shadow-sky-300 drop-shadow drop-shadow-red-300 flex flex-col`}>
+                                        <div className={`${value.color1} bg-[url('/box.png')] bg-blend-overlay flex flex-col gap-5 rounded-t-xl w-80 h-45 p-5`}>
+                                            <div className={`size-11 flex-shrink-0 flex items-center justify-center rounded-full bg-white/40 text-white`}>{value.icon}</div>
+                                            <div className={`flex flex-col gap-2`}>
+                                                <h2 className={`font-bold text-lg text-white/90`}>{value.title}</h2>
+                                                <p className={`text-sm h-10 text-white/80`}>{value.description}</p>
+                                            </div>
+                                        </div>
+                                        <Link href={value.link} className={`h-16 rounded-b-xl w-80 bg-white py-3 px-4`}>
+                                            <div className={` ${value.color2} rounded-lg cursor-pointer ${value.textColor1} flex items-center justify-center text-lg size-full`}>مشاهده</div>
+                                        </Link>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    )
+                })}
+            </div>
         </div>
+
     );
 };
 
